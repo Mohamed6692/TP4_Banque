729ff80c3d5de01e604ebbea2661754b4a996ac0
Mohamed <mohamedcoulibaly66201@gmail.com>
2023-11-12 23:04:45 +0100

    javadoc

--- src/main/java/com/mohamed/tpbanquemohamed/entity/CompteBancaire.java
+++ src/main/java/com/mohamed/tpbanquemohamed/entity/CompteBancaire.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package com.mohamed.tpbanquemohamed.entity;
 
 import jakarta.persistence.CascadeType;
@@ -17,11 +18,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
+/**
  * Cette classe représente un compte bancaire avec des propriétés telles que l'identifiant, le nom du titulaire et le solde.
  * Elle fournit des méthodes pour déposer et retirer de l'argent, ainsi que des méthodes de base pour la gestion du compte.
+ *
+ * L'entité est annotée avec @Entity pour indiquer qu'elle est une entité JPA.
+ * Elle implémente l'interface Serializable pour permettre la sérialisation.
+ * La classe utilise des requêtes nommées (NamedQueries) pour effectuer des requêtes spécifiques dans la base de données.
  */
-
 @Entity
 @NamedQueries({
     @NamedQuery(name = "CompteBancaire.findAll", query = "SELECT c FROM CompteBancaire c"),
@@ -41,61 +45,113 @@ public class CompteBancaire implements Serializable {
     private String nom;
     private int solde;
 
+    /**
+     * Constructeur par défaut nécessaire pour JPA.
+     * Ajoute automatiquement une opération de création du compte.
+     */
     public CompteBancaire() {
-        // Ajout d'une opération pour la création du compte
         operations.add(new OperationBancaire("Création du compte", solde));
     }
 
+    /**
+     * Constructeur pour créer un compte avec un nom et un solde initial.
+     * Ajoute automatiquement une opération de création du compte.
+     *
+     * @param nom Nom du titulaire du compte.
+     * @param solde Solde initial du compte.
+     */
     public CompteBancaire(String nom, int solde) {
         this.nom = nom;
         this.solde = solde;
-
-        // Ajout d'une opération pour la création du compte
         operations.add(new OperationBancaire("Création du compte", solde));
     }
 
+    /**
+     * Getter pour l'identifiant du compte.
+     *
+     * @return L'identifiant du compte.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Getter pour le nom du titulaire du compte.
+     *
+     * @return Le nom du titulaire du compte.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Setter pour le nom du titulaire du compte.
+     *
+     * @param nom Le nouveau nom du titulaire du compte.
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Getter pour le solde du compte.
+     *
+     * @return Le solde du compte.
+     */
     public int getSolde() {
         return solde;
     }
 
+    /**
+     * Setter pour le solde du compte.
+     *
+     * @param solde Le nouveau solde du compte.
+     */
     public void setSolde(int solde) {
         this.solde = solde;
     }
 
+    /**
+     * Getter pour la liste des opérations associées au compte.
+     *
+     * @return La liste des opérations du compte.
+     */
     public List<OperationBancaire> getOperations() {
         return operations;
     }
 
+    /**
+     * Méthode pour déposer de l'argent sur le compte.
+     * Ajoute une opération de dépôt à la liste des opérations.
+     *
+     * @param montant Le montant à déposer.
+     */
     public void deposer(int montant) {
         solde += montant;
-
-        // Ajout d'une opération de dépôt
         operations.add(new OperationBancaire("Dépôt", montant));
     }
 
+    /**
+     * Méthode pour retirer de l'argent du compte.
+     * Ajoute une opération de retrait à la liste des opérations.
+     * Le solde ne peut pas devenir négatif.
+     *
+     * @param montant Le montant à retirer.
+     */
     public void retirer(int montant) {
         if (montant < solde) {
             solde -= montant;
-
-            // Ajout d'une opération de retrait
             operations.add(new OperationBancaire("Retrait", montant));
         } else {
             solde = 0;
         }
     }
 
+    /**
+     * Implémentation de la méthode hashCode() pour permettre une utilisation correcte dans les structures de données.
+     *
+     * @return La valeur de hachage de l'objet.
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -103,6 +159,12 @@ public class CompteBancaire implements Serializable {
         return hash;
     }
 
+    /**
+     * Implémentation de la méthode equals() pour permettre la comparaison d'objets.
+     *
+     * @param object L'objet à comparer.
+     * @return True si les objets sont égaux, sinon False.
+     */
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof CompteBancaire)) {
@@ -112,6 +174,11 @@ public class CompteBancaire implements Serializable {
         return (this.id != null || other.id == null) && (this.id == null || this.id.equals(other.id));
     }
 
+    /**
+     * Implémentation de la méthode toString() pour obtenir une représentation textuelle de l'objet.
+     *
+     * @return La représentation textuelle de l'objet.
+     */
     @Override
     public String toString() {
         return "com.mohamed.tpbanquemohamed.entity.CompteBancaire[ id=" + id + " ]";
